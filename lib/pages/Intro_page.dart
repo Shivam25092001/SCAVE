31f61c48f192247671d0d2bcace0cd5a7a914ff6
Shivam <ss056989@gmail.com>
2021-03-20 15:45:03 +0530

    to put custom colors we use ** new Container(color: const Color(0xff2980b9)); ** so modified intro page

--- lib/pages/Intro_page.dart
+++ lib/pages/Intro_page.dart
@@ -4,9 +4,13 @@ class Intro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: const Color(0xffFFCC00),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("Assets/images/Scave.png"),
+              Center(
+                  child: Image.asset("Assets/images/Full_SCAVE.jpg")),
             ],
           ),
     );
